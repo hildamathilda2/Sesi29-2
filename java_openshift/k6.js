9c84a0072b5447a7f6d22e978d280f6bd89701c0
hilda <hilda@email.com>
2025-08-12 09:55:31 +0700

    jenks haproxy

--- java_openshift/k6.js
+++ java_openshift/k6.js
@@ -1,15 +1,20 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-const BASE_URL = 'http://java-openshift-jenkins-hildamathilda7-dev.apps.rm1.0a51.p1.openshiftapps.com'; // Ganti dengan Route kamu
+const BASE_URL = 'http://haproxy-jo-hildamathilda7-dev.apps.rm1.0a51.p1.openshiftapps.com'; // Replace with your actual HAProxy service DNS
 
 export let options = {
   stages: [
-    { duration: '3m', target: 100 },  // 10 users selama 1 menit
+    { duration: '1m', target: 20 },   // ramp-up to 50 VUs
+    { duration: '2m', target: 60 },  // hold at 100 VUs
+    { duration: '1m', target: 0 },    // ramp-down
   ],
 };
 
 export default function () {
-  http.get(BASE_URL);
+  const res = http.get(`${BASE_URL}/status/200`);
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+  });
   sleep(1);
 }
